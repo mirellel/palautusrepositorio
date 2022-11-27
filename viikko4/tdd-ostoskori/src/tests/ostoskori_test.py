d5a68eef5ab69477fce7aa99775dd20c64bb8129
mirelle01 <lybackmirelle@gmail.com>
2022-11-27 20:07:13 +0200

    ostoskori laskee eri tavaroiden määrän

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,3 +21,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavara(self):
+        maito = Tuote("Maito", 3)
+        bisse = Tuote("Olut", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(bisse)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
