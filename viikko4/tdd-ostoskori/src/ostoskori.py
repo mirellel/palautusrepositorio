da2928b0946106cf4631c7933dd63062d8283eb3
mirelle01 <lybackmirelle@gmail.com>
2022-11-27 19:48:12 +0200

    ostoskori palauttaa alussa oikean hinnan ja tavaroiden määrän

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,18 +3,18 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        self.ostokset = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        pass
+        return len(self.ostokset)
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        if len(self.ostokset) == 0:
+            return 0
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
