b49b51807498209da355c7e2dbf801dea642e7a8
mirelle01 <lybackmirelle@gmail.com>
2022-12-06 19:38:37 +0200

    tennis

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -11,47 +11,43 @@ class TennisGame:
         else:
             self.m_score2 = self.m_score2 + 1
 
+
+
     def get_score(self):
-        score = ""
-        temp_score = 0
+        result = self.m_score1 - self.m_score2
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        if result == 0:
+            return self.even_score(self.m_score1)
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if self.m_score1 >= 4 or self.m_score2 >=4:
+            return self.over_forty(result)
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            return self.under_forty()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def even_score(self, score):
+        return {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All"
+        }.get(score, "Deuce")
 
-        return score
+    def over_forty(self, score):
+        if score == 1:
+            return "Advantage Player1"
+        if score == -1:
+            return "Advantage Player2"
+        if score >= 2:
+            return "Win for Player1"
+        else:
+            return "Win for Player2"
+
+    def under_forty(self):
+        dict = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty"
+        }
+        return f"{dict.get(self.m_score1)}-{dict.get(self.m_score2)}"
+
+        
