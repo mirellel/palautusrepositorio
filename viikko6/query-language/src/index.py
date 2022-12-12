d759e85f1555bba8c2e30c29443c09e67b0e8ce7
mirelle01 <lybackmirelle@gmail.com>
2022-12-12 19:34:49 +0200

    tehtävät 1-3

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,23 @@
+from stats import Statistics
+from player_reader import PlayerReader
+from matchers import And, All, HasAtLeast, HasFewerThan, Not, PlaysIn, Or
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+    
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+    )
+)
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
